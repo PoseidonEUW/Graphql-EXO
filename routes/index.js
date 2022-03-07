fb0635d8e888af27da2fff637dacd07eae9020b8
Jessy Provenzal <jessyprovenzal@hotmail.com>
2022-03-07 16:46:09 +0100

    Second commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
+var classtyle = 'md:bg-transparent md:text-blue-700 md:p-0 dark:text-white'
+    /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'ExpressExo' });
+    res.render('index', { title: 'ExpressExo', class: classtyle });
 });
+router.get('/aboutus', function(req, res, next) {
+    res.render('aboutus', { title: 'About us' });
+});
+router.get('/login', function(req, res, next) {
+    res.render('login', { title: 'Login' });
+});
+
+
 
 module.exports = router;
